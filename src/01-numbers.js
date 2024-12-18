29482061453db72cc28a0f9a51f7eccf456a4316
jd richards <jdrichardstech@gmail.com>
2024-12-18 16:49:40 -0500

    update numbers

--- src/01-numbers.js
+++ src/01-numbers.js
@@ -1,23 +1,31 @@
+// This function demonstrates basic addition
+// Instructions: Add two numbers together
 // Input: num1 = 2, num2 = 3
-// Output: sum = 5
+// Output: result = 5
 function addTwoNumbers() {
 
 }
 
+// This function demonstrates squaring a number
+// Instructions: Multiply the number by itself
 // Input: num = 3
-// Output: squared = 9
+// Output: result = 9
 function squareNumber() {
 
 }
 
+// This function calculates the average of three numbers
+// Instructions: Add all three numbers and divide by 3
 // Input: num1 = 1, num2 = 2, num3 = 3 
-// Output: average = 2
+// Output: result = 2
 function calculateAverage() {
 
 }
 
+// This function checks if a number is even
+// Instructions: Return true if the number is divisible by 2
 // Input: num = 4
-// Output: isEven = true
+// Output: result = true
 function isEven() {
 
 }
