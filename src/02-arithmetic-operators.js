0efacb94509d22707a45ea8edb6608ed3f10c2c9
jd richards <jdrichardstech@gmail.com>
2025-01-03 13:10:46 -0500

    worked on project

--- src/02-arithmetic-operators.js
+++ src/02-arithmetic-operators.js
@@ -3,8 +3,8 @@
 // Instructions: Calculate the product of the second and third numbers, then add it to the first number
 // Input: num1 = 5, num2 = 3, num3 = 2
 // Output: result = 11
-function calculateProductSum() {
-
+function calculateProductSum(num1, num2, num3) {
+    return num1 + (num2 * num3);
 }
 
 // This function demonstrates division and subtraction
@@ -12,8 +12,8 @@ function calculateProductSum() {
 // Instructions: Divide the first number by the second number, then subtract the third number
 // Input: num1 = 10, num2 = 2, num3 = 3  
 // Output: result = 2
-function divideAndSubtract() {
-
+function divideAndSubtract(num1, num2, num3) {
+    return (num1 / num2) - num3;
 }
 
 // This function uses all four basic operators
@@ -22,8 +22,8 @@ function divideAndSubtract() {
 //              add the first number, then subtract the fifth number
 // Input: num1 = 5, num2 = 4, num3 = 6, num4 = 3, num5 = 2
 // Output: result = 11
-function complexCalculation() {
-
+function complexCalculation(num1, num2, num3, num4, num5) {
+    return ((num2 * num3) / num4) + num1 - num5;
 }
 
 // This function demonstrates exponents and division
@@ -31,16 +31,16 @@ function complexCalculation() {
 // Instructions: Square the first number, then divide by the second number
 // Input: num1 = 6, num2 = 2
 // Output: result = 18
-function squareAndDivide() {
-
+function squareAndDivide(num1, num2) {
+    return (num1 * num1) / num2;
 }
 
 // This function works with remainders using the modulo operator
 // Instructions: First divide num1 by num2, then find the remainder
 // Input: num1 = 17, num2 = 5
 // Output: result = 2
-function findRemainder() {
-
+function findRemainder(num1, num2) {
+    return num1 % num2;
 }
 
 module.exports = {
--- src/03-strings.js
+++ src/03-strings.js
@@ -2,40 +2,40 @@
 // Instructions: Join the first and second string with a space in between
 // Input: str1 = "Hello", str2 = "World"
 // Output: result = "Hello World"
-function joinStrings() {
-
+function joinStrings(str1, str2) {
+    return str1 + str2;
 }
 
 // This function demonstrates string methods
 // Instructions: Convert the input string to all uppercase letters
 // Input: str = "hello"
 // Output: result = "HELLO"
-function convertToUpperCase() {
-
+function convertToUpperCase(str) {
+    return str.toUpperCas();
 }
 
 // This function demonstrates string slicing
 // Instructions: Extract characters from start index to end index (inclusive)
 // Input: str = "JavaScript", startIndex = 1, endIndex = 3
 // Output: result = "ava"
-function sliceString() {
-
+function sliceString(str, startIndex, endIndex) {
+    return str.slice(startIndex, endIndex);
 }
 
 // This function demonstrates string searching
 // Instructions: Find if the search string exists in the main string
 // Input: mainStr = "Hello World", searchStr = "World"
 // Output: result = true
-function containsSubstring() {
-
+function containsSubstring(mainStr, searchStr) {
+    return mainStr.includes(searchStr);
 }
 
 // This function demonstrates string replacement
 // Instructions: Replace all occurrences of oldChar with newChar
 // Input: str = "hello hello", oldChar = "l", newChar = "w"
 // Output: result = "hewwo hewwo"
-function replaceCharacters() {
-
+function replaceCharacters(str, oldChar, newChar) {
+    return str.replace(oldChar, newChar);
 }
 
 module.exports = {
